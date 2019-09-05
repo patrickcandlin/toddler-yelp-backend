29345c6cd8104ae34f8ee300b76180bf9e923a2c
“Patrick <pctennislessons@gmail.com>
2019-09-05 14:04:15 -0600

    search form works and params are received

--- app/controllers/fusion_controller.rb
+++ app/controllers/fusion_controller.rb
@@ -1,5 +1,7 @@
 class FusionController < ApplicationController
   def index
+    # byebug
+    puts params['term']
     @API_KEY = ENV['fusion_api_key']
     @API_HOST = "https://api.yelp.com"
     @SEARCH_PATH = "/v3/businesses/search"
@@ -7,21 +9,32 @@ class FusionController < ApplicationController
     @DEFAULT_BUSINESS_ID = "yelp-san-francisco"
     @DEFAULT_TERM = "dinner"
     @DEFAULT_LOCATION = "Denver, CO"
-    @SEARCH_LIMIT = 5
+    @SEARCH_LIMIT = 20
     
     def search(term, location)
         url = "https://api.yelp.com/v3/businesses/search"
         params = {
           term: term,
           location: location,
-          limit: 10
+          limit: @SEARCH_LIMIT
         }
 
-        response = HTTP.auth("Bearer #{ENV['fusion_api_key']}").get(url, params: params)
+        response = HTTP
+          .auth("Bearer #{ENV['fusion_api_key']}")
+          .get(url, params: params)
         response.parse
     end
     
-    render json: search("dinner", "Denver, CO")
+    render json: search(params['term'], params['location'])
 
   end
+
+
+
+  # private
+  #   def fusion_params
+  #     params.require(:fusions).permit(:term, location)
+  #   end
 end
+
+ActionController::Parameters.permit_all_parameters = true
