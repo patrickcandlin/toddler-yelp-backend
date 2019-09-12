d5f3a6c04b5571d23ae1c3c63a4f079a782f820c
“Patrick <pctennislessons@gmail.com>
2019-09-12 11:16:39 -0600

    reviews functionality implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
+    has_many :reviews
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
     validates :password, :confirmation => true,
-                       :length => {:within => 6..40},
                        :allow_blank => true,
                        :on => :update
     validates :password, :confirmation => true,
-                       :length => {:within => 6..40},
                        :allow_blank => true,
                        :on => :update
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_021347) do
+ActiveRecord::Schema.define(version: 2019_09_11_171604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "yelp_id"
+    t.string "yelp_name"
+    t.integer "rating"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 2019_09_10_021347) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "users"
 end
